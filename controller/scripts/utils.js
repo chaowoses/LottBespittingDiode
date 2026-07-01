202a0501864b5e5303679605f7e887586f71aa78
chaowoses <chaowoses@users.noreply.github.com>
2026-07-01 02:44:50 +0000

    deploy: 61cb1cbfe4e711ee56207f3974b8c16a73382271

---
+++ controller/scripts/utils.js
@@ -0,0 +1,2 @@
+function escHtml(s){ return String(s||'').replace(/&/g,'&amp;').replace(/</g,'&lt;').replace(/>/g,'&gt;').replace(/"/g,'&quot;'); }
+function escAttr(s){ return String(s||'').replace(/'/g,'&#39;').replace(/"/g,'&quot;'); }
